d0d981e0603ee5294029e1c94eb40df868361465
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-27 15:42:07 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 01 - AUTO PROPERTIES/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCISE_01___AUTO_PROPERTIES {
+    //------------------------------- START -------------------------------//
+    class Product {
+        private string _name;
+        public double Price { get; private set; }
+        public int Amount { get; private set; }
+        public Product() {
+        }
+        public Product(string name, double price, int amount) {
+            _name = name;
+            Price = price;
+            Amount = amount;
+        }
+        public string Name {
+            get { return _name; }
+            set { if (value != null && value.Length > 1) {
+                    _name = value;
+                }
+            }
+        }       
+         
+        public double AmountValueStock() {
+            return Price * Amount;
+        }
+        public void AddProducts(int quantidade) {
+            Amount += quantidade;
+        }
+        public void RemoveProducts(int quantidade) {
+            Amount -= quantidade;
+        }
+        public override string ToString() {
+            return _name
+            + ", $ " + Price.ToString("F2") + ", " + Amount + " unidades, Total: $ " + AmountValueStock().ToString("F2");
+        }
+    }
+    //-------------------------------- END -------------------------------// 
+}
